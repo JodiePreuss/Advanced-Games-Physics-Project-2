51038ed4219260662e21d8d6dcef7858b031ea17
vbondarenko1 <vbondarenko@babbel.com>
2019-06-08 12:53:02 +0200

    fire shader graph created

--- GTAT 2/Assets/Scripts/myMesh.cs
+++ GTAT 2/Assets/Scripts/myMesh.cs
@@ -52,25 +52,25 @@ public class myMesh : MonoBehaviour
             }
         }
 
-        triangles = new int[6 * (horizontalLines + 1) * (verticalLines + 1)];
-        int vert = 0;
-        int tris = 0;
-        for (int z = 0; z < verticalLines; z++)
-        {
-            for (int x = 0; x < horizontalLines; x++)
-            {
+        //triangles = new int[6 * (horizontalLines + 1) * (verticalLines + 1)];
+        //int vert = 0;
+        //int tris = 0;
+        //for (int z = 0; z < verticalLines; z++)
+        //{
+        //    for (int x = 0; x < horizontalLines; x++)
+        //    {
 
-                triangles[tris + 0] = vert + 0;
-                triangles[tris + 1] = vert + horizontalLines + 1;
-                triangles[tris + 2] = vert + 1;
-                triangles[tris + 3] = vert + 1;
-                triangles[tris + 4] = vert + horizontalLines + 1;
-                triangles[tris + 5] = vert + horizontalLines + 2;
-                vert++;
-                tris += 6;
-            }
+        //        triangles[tris + 0] = vert + 0;
+        //        triangles[tris + 1] = vert + horizontalLines + 1;
+        //        triangles[tris + 2] = vert + 1;
+        //        triangles[tris + 3] = vert + 1;
+        //        triangles[tris + 4] = vert + horizontalLines + 1;
+        //        triangles[tris + 5] = vert + horizontalLines + 2;
+        //        vert++;
+        //        tris += 6;
+        //    }
 
-        }
+        //}
 
     }
 
