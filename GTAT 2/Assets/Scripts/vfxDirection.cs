1b631f3f1a6d4d26aa475167ecfe567a8ef2497b
vbondarenko1 <vbondarenko@babbel.com>
2019-06-09 15:02:36 +0200

    Fixes

---
+++ GTAT 2/Assets/Scripts/vfxDirection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class vfxDirection : MonoBehaviour
+{
+    public float t;
+    private Vector3 setDirection;
+    private GameObject vfx;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //vfx = this.gameObject;
+
+        //var VF = GetComponent<VisualEffectAssetEditor>();
+
+        //var Direction = VisualEffect.GetVector3("Direction");
+
+        //Direction = QuadraticCurve();
+
+
+    }
+
+
+    Vector3 QuadraticCurve() {
+        return new Vector3(t, t*t, 0);
+    }
+}
